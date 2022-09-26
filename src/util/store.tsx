cde0ff0a7322a871fcb3b3d199b95ca85416ee2a
Orca-Itgit config --global user.email whitefox@kakao.com <whitefox@kakao.com>
2022-09-26 20:17:26 +0900

    브랜치 병합 완료 및 리덕스 코드 수정

--- src/util/store.tsx
+++ src/util/store.tsx
@@ -1,13 +1,13 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
-const initialState = JSON.parse(localStorage.getItem('todo'));
+const initialState = JSON.parse(localStorage.getItem('todo') || "");
 
 let todo = createSlice({
     name : 'todo',
     initialState,
     reducers : {
         remove(state, idx){
-            let copy =  state.filter((item, index) => {
+            let copy =  state.filter((item:any, index:any) => {
                 return index != idx.payload;
             })
             localStorage.setItem('todo', JSON.stringify(copy));
